0fa0a9cd93d120a2c8a4f6ae83097e8043642ee8
bradyvolk <39806319+bradyvolk@users.noreply.github.com>
2022-09-04 14:29:13 -0400

    Make overlay have blue-purple hue

--- components/Home/tab-content/Projects.vue
+++ components/Home/tab-content/Projects.vue
@@ -12,7 +12,7 @@
       <b-carousel
         class="projects-carousel"
         v-model="slide"
-        :interval="4000"
+        :interval="8000"
         controls
         indicators
         background="#ababab"
@@ -21,47 +21,50 @@
       >
         <b-carousel-slide img-height="100px">
           <h2>This Site</h2>
-          <!-- @TODO change overlay to light grey blue instead of low-bright -->
           <template #img>
-            <a
-              href="https://github.com/bradyvolk/personal-website"
-              target="_blank"
-            >
-              <img
-                class="d-block img-fluid w-100 low-bright"
-                src="this-site.png"
-                alt="screenshot of this website"
-              />
-            </a>
+            <div class="image-overlay">
+              <a
+                href="https://github.com/bradyvolk/personal-website"
+                target="_blank"
+              >
+                <img
+                  class="d-block img-fluid w-100 img-fade"
+                  src="this-site.png"
+                  alt="screenshot of this website"
+                />
+              </a>
+            </div>
           </template>
         </b-carousel-slide>
-
         <b-carousel-slide img-height="100px">
           <h2>Cornell Digital Agriculture Website</h2>
           <template #img>
-            <a
-              href="https://github.com/bradyvolk/ewb-dig-ag-site"
-              target="_blank"
-            >
-              <img
-                class="d-block img-fluid w-100 low-bright"
-                src="dig-ag-site.png"
-                alt="screenshot of ewb-dig-ag.org website"
-              />
-            </a>
+            <div class="image-overlay">
+              <a
+                href="https://github.com/bradyvolk/ewb-dig-ag-site"
+                target="_blank"
+              >
+                <img
+                  class="d-block img-fluid w-100 img-fade"
+                  src="dig-ag-site.png"
+                  alt="screenshot of ewb-dig-ag.org website"
+                />
+              </a>
+            </div>
           </template>
         </b-carousel-slide>
-
         <b-carousel-slide img-height="100px">
           <h2>Cornell Digital Agriculture Desktop App</h2>
           <template #img>
-            <a href="https://github.com/bradyvolk/ewb-gui" target="_blank">
-              <img
-                class="d-block img-fluid w-100 low-bright"
-                src="dig-ag-desktop-app.png"
-                alt="screenshot of EWB dig ag desktop app"
-              />
-            </a>
+            <div class="image-overlay">
+              <a href="https://github.com/bradyvolk/ewb-gui" target="_blank">
+                <img
+                  class="d-block img-fluid w-100 img-fade"
+                  src="dig-ag-desktop-app.png"
+                  alt="screenshot of EWB dig ag desktop app"
+                />
+              </a>
+            </div>
           </template>
         </b-carousel-slide>
       </b-carousel>
@@ -99,8 +102,12 @@ export default {
   padding-bottom: 0.5rem;
 }
 
-.low-bright {
-  filter: brightness(55%);
+.img-fade {
+  opacity: 65%;
+}
+
+.image-overlay {
+  background-color: #132b3f;
 }
 
 :deep(.carousel-item) {
