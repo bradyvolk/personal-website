ae73db13f750d1491694408d462662d2395da875
bradyvolk <39806319+bradyvolk@users.noreply.github.com>
2022-10-03 22:47:30 -0400

    Update static

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "personal-website",
+    title: "Brady Volkmann",
     htmlAttrs: {
       lang: "en",
     },
